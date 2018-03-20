6f40ac7f90dbc1c232615a09d84e466413f9041b
Keshane <keshanegan@gmail.com>
2018-03-20 01:58:33 -0400

    Added contact information

--- keshane_site/views.py
+++ keshane_site/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'keshane_site/base.html')
+    return render(request, 'keshane_site/index.html')
+
+
+def contact(request):
+    return render(request, 'keshane_site/contact.html')
