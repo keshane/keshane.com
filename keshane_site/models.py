6301ad7945a9ffb257b0c70268c161367367bbef
Keshane <keshanegan@gmail.com>
2018-07-20 00:13:26 -0400

    Added functionality to notify subscribers when a post is created

--- keshane_site/models.py
+++ keshane_site/models.py
@@ -1,3 +1,5 @@
+from django.core.mail import send_mass_mail
+from django.urls import reverse
 from django.db import models
 
 
@@ -16,6 +18,27 @@ class Post(models.Model):
     date_added = models.DateTimeField(auto_now=True)
     date_modified = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
+
+    def save(self, *args, **kwargs):
+        """Adds custom behavior to notify subscribers when a post has been created"""
+        if self._state.adding:
+            self.notify_subscribers()
+        super().save(*args, **kwargs)
+
+    # TODO put messages in database
+    def notify_subscribers(self):
+        """Lets readers subscribed to the blog about a change"""
+        subject = "New post on Keshane's blog about " + self.title
+        message = "Check out Keshane's insights at https://keshane.com" + reverse("blog") + "!"
+
+        emails = tuple(
+            (subject,
+             "Dear " + subscriber.first_name + ",\r\n" + message,
+             "blog@keshane.com",
+             [subscriber.email_address])
+            for subscriber in Subscriber.objects.all())
+
+        send_mass_mail(emails)
 
     def __str__(self):
         return self.title
