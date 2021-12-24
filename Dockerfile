a15c8a1910785ef5b74aa07121f57621eeaf76b5
keshane <keshanegan@gmail.com>
2021-12-23 23:07:46 -0500

    Add dummy settings and wsgi to repo
    Also add a dummy db.sqlite3 file in the docker image

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ RUN pip3 install -r requirements.txt
 RUN pip3 install gunicorn
 
 COPY . .
+
+RUN touch db.sqlite3
 RUN python manage.py migrate --no-input
-RUN cat db.sqlite3
 CMD ["gunicorn", "keshane_com.wsgi", "-b", "0:8000", "--reload"]
---
+++ keshane_com/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for keshane_com project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "keshane_com.settings")
+
+application = get_wsgi_application()
+
